3ac749e8151e5e6721d5adf92928828afcb0ef50
Jeremiah Chinwe <chinwejeremiah9@gmail.com>
2023-10-20 23:00:55 +0100

    I updated the app

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -5,20 +5,20 @@ export default function About() {
     <section className="px-[1.5rem]" name="about">
         <h1 className="font-[600] pt-20 text-[2.25rem] text-center pb-2 border-b-2 border-[#222222] w-fit my-0 mx-auto">About</h1>
 
-        <p className="leading-[1.5rem] font-[400] text-center mx-auto block sm:w-[80%] lg:w-[50%]">I&apos;m a front-end software developer with experience building visually appealing, mobile responsive, high-functional, maintainable, and scalable web apps with modern technologies that meets your business expectations, provides real-world business solutions within your budget and on time, and while satisfying your end users.</p>
+        <p className="leading-[1.5rem] mt-8 font-[400] text-center mx-auto block sm:w-[80%] lg:w-[50%]">I&apos;m a front-end software developer with experience building visually appealing, mobile responsive, high-functional, maintainable, and scalable web apps with modern technologies that meets your business expectations, provides real-world business solutions within your budget and on time, and while satisfying your end users.</p>
 
         <div className="my-12 flex items-center justify-center flex-wrap gap-4 w-fit mx-auto lg:w-[70%]">
-            <button className="border-none bg-[#f5f4f4] text-[#222222] font-[600] py-1 px-4 rounded-lg text-lg cursor-default">HTML</button>
-            <button className="border-none bg-[#f5f4f4] text-[#222222] font-[600] py-1 px-4 rounded-lg text-lg cursor-default">CSS</button>
-            <button className="border-none bg-[#f5f4f4] text-[#222222] font-[600] py-1 px-4 rounded-lg text-lg cursor-default">Tailwindcss</button>
-            <button className="border-none bg-[#f5f4f4] text-[#222222] font-[600] py-1 px-4 rounded-lg text-lg cursor-default">SASS</button>
-            <button className="border-none bg-[#f5f4f4] text-[#222222] font-[600] py-1 px-4 rounded-lg text-lg cursor-default">JavaScript</button>
-            <button className="border-none bg-[#f5f4f4] text-[#222222] font-[600] py-1 px-4 rounded-lg text-lg cursor-default">React.js</button>
-            <button className="border-none bg-[#f5f4f4] text-[#222222] font-[600] py-1 px-4 rounded-lg text-lg cursor-default">Next.js</button>
-            <button className="border-none bg-[#f5f4f4] text-[#222222] font-[600] py-1 px-4 rounded-lg text-lg cursor-default">TypeScript</button>
-            <button className="border-none bg-[#f5f4f4] text-[#222222] font-[600] py-1 px-4 rounded-lg text-lg cursor-default">Redux Toolkit</button>
-            <button className="border-none bg-[#f5f4f4] text-[#222222] font-[600] py-1 px-4 rounded-lg text-lg cursor-default">API Intergration</button>
-            <button className="border-none bg-[#f5f4f4] text-[#222222] font-[600] py-1 px-4 rounded-lg text-lg cursor-default">Firebase</button>
+            <button className="border-none bg-[#f5f4f4] text-[#222222] font-[600] py-1 px-4 rounded-lg text-sm cursor-default">HTML</button>
+            <button className="border-none bg-[#f5f4f4] text-[#222222] font-[600] py-1 px-4 rounded-lg text-sm cursor-default">CSS</button>
+            <button className="border-none bg-[#f5f4f4] text-[#222222] font-[600] py-1 px-4 rounded-lg text-sm cursor-default">Tailwindcss</button>
+            <button className="border-none bg-[#f5f4f4] text-[#222222] font-[600] py-1 px-4 rounded-lg text-sm cursor-default">SASS</button>
+            <button className="border-none bg-[#f5f4f4] text-[#222222] font-[600] py-1 px-4 rounded-lg text-sm cursor-default">JavaScript</button>
+            <button className="border-none bg-[#f5f4f4] text-[#222222] font-[600] py-1 px-4 rounded-lg text-sm cursor-default">React.js</button>
+            <button className="border-none bg-[#f5f4f4] text-[#222222] font-[600] py-1 px-4 rounded-lg text-sm cursor-default">Next.js</button>
+            <button className="border-none bg-[#f5f4f4] text-[#222222] font-[600] py-1 px-4 rounded-lg text-sm cursor-default">TypeScript</button>
+            <button className="border-none bg-[#f5f4f4] text-[#222222] font-[600] py-1 px-4 rounded-lg text-sm cursor-default">Redux Toolkit</button>
+            <button className="border-none bg-[#f5f4f4] text-[#222222] font-[600] py-1 px-4 rounded-lg text-sm cursor-default">API Intergration</button>
+            <button className="border-none bg-[#f5f4f4] text-[#222222] font-[600] py-1 px-4 rounded-lg text-sm cursor-default">Firebase</button>
         </div>
     </section>
   )
