8f2e21f1a2a6b6cf37bef6b6f96a0f732bdf51c7
Jeremiah Chinwe <chinwejeremiah9@gmail.com>
2023-11-12 23:33:30 +0100

    I updated the project component

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -5,7 +5,7 @@ export default function About() {
     <section className="px-[1.5rem]" name="about">
         <h1 className="font-[600] pt-20 text-[2.25rem] text-center pb-2 border-b-2 border-[#222222] w-fit my-0 mx-auto">About</h1>
 
-        <p className="leading-[1.5rem] mt-8 font-[400] text-center mx-auto block sm:w-[80%] lg:w-[50%]">I&apos;m a front-end software developer with experience building visually appealing, mobile responsive, high-functional, maintainable, and scalable web apps with modern technologies that meets your business expectations, provides real-world business solutions within your budget and on time, and while satisfying your end users.</p>
+        <p className="leading-[1.75rem] mt-8 font-[400] text-center mx-auto block sm:w-[80%] lg:w-[50%]">I&apos;m a front-end software developer with experience building visually appealing, mobile responsive, high-functional, maintainable, and scalable web apps with modern technologies with the aim of meeting your business expectations within your budget and on time while satisfying your end users.</p>
 
         <div className="my-12 flex items-center justify-center flex-wrap gap-4 w-fit mx-auto lg:w-[70%]">
             <button className="border-none bg-[#f5f4f4] text-[#222222] font-[600] py-1 px-4 rounded-lg text-sm cursor-default">HTML</button>
--- src/library/Library.jsx
+++ src/library/Library.jsx
@@ -1,7 +1,9 @@
 import blockWallet from "./assets/block_wallet_updated.png"
+import getlinked from "./assets/getlinked_desktop.png"
+import shopping from "./assets/recent_shopping.png"
 import techTime from "./assets/screenshot_desktop.png"
 import fichat from "./assets/updated-dashboard-fitesite.png"
-import shopping from "./assets/recent_shopping.png"
+
 
 export const projects = [
     {
@@ -24,20 +26,22 @@ export const projects = [
     },
     {
         id: "2",
-        image: techTime,
-        title: "TechTime Website",
-        description: "This is the landing page of the TechTime website where you can purchase all digital courses for all kinds of digital skills. It harnesses the power of react in creating reuseable and maintable functional components for scalability.",
+        image: getlinked,
+        title: "Gelinked Tech Hackathon",
+        description: "The official gelinked landing page having two other pages - the contact & registration page built with firebase (firestore).",
         stacks: [
             { id: "1", name: "HTML" },
             { id: "2", name: "Tailwindcss" },
-            { id: "3", name: "JavaScript" },
-            { id: "4", name: "React.js" },
+            { id: "3", name: "React.js" },
+            { id: "4", name: "Material UI" },
+            { id: "5", name: "Firebase" },
+            { id: "6", name: "Framer Motion" },
         ],
         links: [
             {
                 id: "1", 
-                repo_link: "https://github.com/yesjeremiahchinwe/techtime_landing_page",
-                live_link: "https://techtime-landing-page-neon.vercel.app/"
+                repo_link: "https://github.com/yesjeremiahchinwe/getlinked",
+                live_link: "https://getlinked-mu.vercel.app/"
             },
         ]
     },
@@ -63,9 +67,28 @@ export const projects = [
     },
     {
         id: "4",
+        image: techTime,
+        title: "TechTime Landing Page",
+        description: "This is the landing page of the TechTime website where you can purchase all digital courses for all kinds of digital skills. It harnesses the power of react in creating reuseable and maintable functional components for scalability.",
+        stacks: [
+            { id: "1", name: "HTML" },
+            { id: "2", name: "CSS" },
+            { id: "3", name: "JavaScript" },
+            { id: "4", name: "React.js" }
+        ],
+        links: [
+            {
+                id: "1", 
+                repo_link: "https://github.com/yesjeremiahchinwe/techtime_landing_page",
+                live_link: "https://techtime-landing-page-neon.vercel.app/"
+            }
+        ]
+    },
+    {
+        id: "5",
         image: fichat,
         title: "FiChat App",
-        description: "Fichat is a chat app built during one of my internship programs at Fichub Internship. It was built by a team of both frontend and backend devs. I led the frontend team of three (3) to create visually appealing user interfaces, while working closely with the backend team to implement and consume the api endpoints. Please note that this project is still under development and the link to the github repo may not work because it's an organisation(private) repository.",
+        description: "Fichat is an on-going chat app project. I volunteered to lead the frontend team of three (3) while working closely with the backend team to implement and consume the API endpoints. Please note that this project is still under development and currently, it's only the onboading page and the chat dashboard which you're seeing now is working. (I built the dashboard myself). Also note that the link to the github repo may not work because it's a private repository.",
         stacks: [
             { id: "1", name: "HTML" },
             { id: "2", name: "Tailwindcss" },
