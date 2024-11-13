dcec83f4faa1dd0dc2e5e42e2ae5143412809d68
Jeremiah Chinwe <jeremiahchinwe52@gmail.com>
2024-11-13 16:04:51 +0100

    Modified the hero and about components

--- src/components/hero/Hero.jsx
+++ src/components/hero/Hero.jsx
@@ -4,7 +4,7 @@ export default function Hero() {
     return (
         <section className="flex flex-col justify-center items-center p-[1.25rem]" name="home">
             <h2 className="font-[600] text-[3rem] text-center mt-20">Jeremiah Chinwe</h2>
-            <p className="font-[400] text-center my-4 block sm:w-[80%] lg:w-[50%]">Passionate about excellence and mastery in my field of endeavour with strong attention to details.</p>
+            <p className="font-[400] text-center my-4 block sm:w-[80%] lg:w-[50%]">A greate business with an engaging user-facing interface will loose a great number of its potential customers.</p>
 
             <div className="flex gap-4 sm:gap-8 items-center my-4">
                 <Link to="projects" smooth={true} duration={500} className="block w-fit border-[1px] border-[#333] rounded-lg py-1 px-8 font-[600] hover:bg-[#222222] hover:text-[#ffffff] transition-all duration-500 cursor-pointer">Projects</Link>
